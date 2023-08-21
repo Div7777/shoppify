6fa6c99d2478055f13ae8c80b2731d5d5afde986
div7777 <diveshkumarbhoi@gmail.com>
2023-08-22 04:46:52 +0530

    creating

---
+++ my-app/src/App1p.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App1p = () => {
+    return (
+        <div>
+            <header>
+                <a href='/'>shoppify</a>
+            </header>
+            <main>
+                list product
+            </main>
+        </div>
+    )
+}
+
+export default App1p
---
+++ my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App1p';
+// import App from './material_UI/App';
+import './indexx.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+
